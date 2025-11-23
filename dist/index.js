000b8a76fdcc6cd7bb2e05a94c631e66de156656
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-11-23 10:47:24 +0000

    added readiness handler

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 import express from "express";
 const app = express();
-app.listen(8080);
+const PORT = 8080;
+app.use("/app", express.static("./src/app"));
+app.get("/healthz", handlerReadiness);
+function handlerReadiness(req, res) {
+    res.set({ "Content-type": "text/plain" });
+    res.send("OK");
+}
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
